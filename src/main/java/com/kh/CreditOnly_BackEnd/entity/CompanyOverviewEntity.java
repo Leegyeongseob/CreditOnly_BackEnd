9e1cdba327010a1bfe016f2b98e507d25d412dde
Lee_gyeong_seob <can34879@gmail.com>
2024-08-30 06:20:17 +0900

    챗봇 수정

--- src/main/java/com/kh/CreditOnly_BackEnd/entity/CompanyOverviewEntity.java
+++ src/main/java/com/kh/CreditOnly_BackEnd/entity/CompanyOverviewEntity.java
@@ -3,6 +3,7 @@ package com.kh.CreditOnly_BackEnd.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -12,29 +13,76 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CompanyOverviewEntity {
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "_id")
+    @Column(name = "id")
     private Long id;
-
-    @Column(name = "CONTENT", columnDefinition = "TEXT")
-    private String content;
-
-    @Column(name = "CONTENT_keyword")
-    private String contentKeyword;
-
-    @Column(name = "WORD")
-    private String word;
-
-    @Column(name = "WORD_keyword")
-    private String wordKeyword;
 
     @Column(name = "_index")
     private String index;
 
     @Column(name = "_score")
-    private int score;
+    private Double score;
 
     @Column(name = "_type")
     private String type;
+
+    @Column(name = "acc_mt")
+    private Integer accMt;
+
+    @Column(name = "adres")
+    private String adres;
+
+    @Column(name = "bizr_no")
+    private String bizrNo;
+
+    @Column(name = "ceo_nm")
+    private String ceoNm;
+
+    @Column(name = "corp_cls")
+    private String corpCls;
+
+    @Column(name = "corp_code")
+    private String corpCode;
+
+    @Column(name = "corp_name")
+    private String corpName;
+
+    @Column(name = "corp_name_eng")
+    private String corpNameEng;
+
+    @Column(name = "est_dt")
+    private LocalDate estDt;
+
+    @Column(name = "fax_no")
+    private String faxNo;
+
+    @Column(name = "hm_url")
+    private String hmUrl;
+
+    @Column(name = "induty_code")
+    private String indutyCode;
+
+    @Column(name = "ir_url")
+    private String irUrl;
+
+    @Column(name = "jurir_no")
+    private String jurirNo;
+
+    @Column(name = "message")
+    private String message;
+
+    @Column(name = "phn_no")
+    private String phnNo;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "stock_code")
+    private String stockCode;
+
+    @Column(name = "stock_name")
+    private String stockName;
 }
--- src/main/java/com/kh/CreditOnly_BackEnd/entity/FinancialCompanyEntity.java
+++ src/main/java/com/kh/CreditOnly_BackEnd/entity/FinancialCompanyEntity.java
@@ -3,7 +3,6 @@ package com.kh.CreditOnly_BackEnd.entity;
 import lombok.*;
 
 import javax.persistence.*;
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
@@ -16,222 +15,18 @@ import java.time.LocalDate;
 public class FinancialCompanyEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "_id")
+    @Column(name = "id")
     private Long id;
 
-    @Column(name = "_index")
-    private String index;
-
-    @Column(name = "_score")
-    private String score;
-
-    @Column(name = "_type")
-    private String type;
-
-    @Column(name = "actnAudpnNm")
-    private String actnAudpnNm;
-
-    @Column(name = "actnAudpnNm_keyword")
-    private String actnAudpnNmKeyword;
-
-    @Column(name = "audtRptOpnnCtt")
-    private String audtRptOpnnCtt;
-
-    @Column(name = "audtRptOpnnCtt_keyword")
-    private String audtRptOpnnCttKeyword;
-
-    @Column(name = "basDt")
-    private LocalDate basDt;
-
-    @Column(name = "basDt_keyword")
-    private String basDtKeyword;
-
-    @Column(name = "bzno")
-    private Long bzno;
-
-    @Column(name = "bzno_keyword")
-    private String bznoKeyword;
-
-    @Column(name = "corpRegMrktDcd")
-    private String corpRegMrktDcd;
-
-    @Column(name = "corpRegMrktDcdNm")
-    private String corpRegMrktDcdNm;
-
-    @Column(name = "corpRegMrktDcdNm_keyword")
-    private String corpRegMrktDcdNmKeyword;
-
-    @Column(name = "crno")
-    private String crno;
-
-    @Column(name = "crno_keyword")
-    private String crnoKeyword;
-
-    @Column(name = "empeAvgCnwkTermCtt")
-    private String empeAvgCnwkTermCtt;
-
-    @Column(name = "empeAvgCnwkTermCtt_keyword")
-    private String empeAvgCnwkTermCttKeyword;
-
-    @Column(name = "fncoAdr")
-    private String fncoAdr;
-
-    @Column(name = "fncoAdr_keyword")
-    private String fncoAdrKeyword;
-
-    @Column(name = "fncoEmpeAvgSlryAmt")
-    private BigDecimal fncoEmpeAvgSlryAmt;
-
-    @Column(name = "fncoEmpeAvgSlryAmt_keyword")
-    private String fncoEmpeAvgSlryAmtKeyword;
-
-    @Column(name = "fncoEmpeCnt")
-    private Integer fncoEmpeCnt;
-
-    @Column(name = "fncoEmpeCnt_keyword")
-    private String fncoEmpeCntKeyword;
-
-    @Column(name = "fncoEnsnNm")
-    private String fncoEnsnNm;
-
-    @Column(name = "fncoEnsnNm_keyword")
-    private String fncoEnsnNmKeyword;
-
-    @Column(name = "fncoEstbDt")
-    private LocalDate fncoEstbDt;
-
-    @Column(name = "fncoEstbDt_keyword")
-    private String fncoEstbDtKeyword;
-
-    @Column(name = "fncoFxno")
-    private String fncoFxno;
-
-    @Column(name = "fncoFxno_keyword")
-    private String fncoFxnoKeyword;
-
-    @Column(name = "fncoHmpgUrl")
-    private String fncoHmpgUrl;
-
-    @Column(name = "fncoHmpgUrl_keyword")
-    private String fncoHmpgUrlKeyword;
-
-    @Column(name = "fncoKosdaqLstgAbolDt")
-    private LocalDate fncoKosdaqLstgAbolDt;
-
-    @Column(name = "fncoKosdaqLstgAbolDt_keyword")
-    private String fncoKosdaqLstgAbolDtKeyword;
-
-    @Column(name = "fncoKosdaqLstgDt")
-    private LocalDate fncoKosdaqLstgDt;
-
-    @Column(name = "fncoKosdaqLstgDt_keyword")
-    private String fncoKosdaqLstgDtKeyword;
-
-    @Column(name = "fncoKrxLstgAbolDt")
-    private LocalDate fncoKrxLstgAbolDt;
-
-    @Column(name = "fncoKrxLstgAbolDt_keyword")
-    private String fncoKrxLstgAbolDtKeyword;
-
-    @Column(name = "fncoKrxLstgDt")
-    private LocalDate fncoKrxLstgDt;
-
-    @Column(name = "fncoKrxLstgDt_keyword")
-    private String fncoKrxLstgDtKeyword;
-
-    @Column(name = "fncoMainBizNm")
-    private String fncoMainBizNm;
-
-    @Column(name = "fncoMainBizNm_keyword")
-    private String fncoMainBizNmKeyword;
-
-    @Column(name = "fncoNm")
+    @Column(name = "fnco_nm")
     private String fncoNm;
 
-    @Column(name = "fncoRprNm")
+    @Column(name = "fnco_addr")
+    private String fncoAddr;
+
+    @Column(name = "fnco_rpr_nm")
     private String fncoRprNm;
 
-    @Column(name = "fncoRprNm_keyword")
-    private String fncoRprNmKeyword;
-
-    @Column(name = "fncoSmenpYn")
-    private String fncoSmenpYn;
-
-    @Column(name = "fncoSmenpYn_keyword")
-    private String fncoSmenpYnKeyword;
-
-    @Column(name = "fncoStacMm")
-    private String fncoStacMm;
-
-    @Column(name = "fncoStacMm_keyword")
-    private String fncoStacMmKeyword;
-
-    @Column(name = "fncoTlno")
-    private String fncoTlno;
-
-    @Column(name = "fncoTlno_keyword")
-    private String fncoTlnoKeyword;
-
-    @Column(name = "fncoXchgLstgAbolDt")
-    private LocalDate fncoXchgLstgAbolDt;
-
-    @Column(name = "fncoXchgLstgAbolDt_keyword")
-    private String fncoXchgLstgAbolDtKeyword;
-
-    @Column(name = "fncoXchgLstgDt")
-    private LocalDate fncoXchgLstgDt;
-
-    @Column(name = "fncoXchgLstgDt_keyword")
-    private String fncoXchgLstgDtKeyword;
-
-    @Column(name = "fncoZpcd")
-    private String fncoZpcd;
-
-    @Column(name = "fncoZpcd_keyword")
-    private String fncoZpcdKeyword;
-
-    @Column(name = "fssCorpChgDtm")
-    private LocalDate fssCorpChgDtm;
-
-    @Column(name = "fssCorpUnqNo")
-    private Long fssCorpUnqNo;
-
-    @Column(name = "fssCorpUnqNo_keyword")
-    private String fssCorpUnqNoKeyword;
-
-    @Column(name = "isinCd")
-    private String isinCd;
-
-    @Column(name = "isinCd_keyword")
-    private String isinCdKeyword;
-
-    @Column(name = "isinCdNm")
-    private String isinCdNm;
-
-    @Column(name = "isinCdNm_keyword")
-    private String isinCdNmKeyword;
-
-    @Column(name = "mntrFcnFncoCd")
-    private String mntrFcnFncoCd;
-
-    @Column(name = "mntrFcnFncoCd_keyword")
-    private String mntrFcnFncoCdKeyword;
-
-    @Column(name = "mntrFcnFncoCdNm")
-    private String mntrFcnFncoCdNm;
-
-    @Column(name = "mntrFcnFncoCdNm_keyword")
-    private String mntrFcnFncoCdNmKeyword;
-
-    @Column(name = "sicCd")
-    private String sicCd;
-
-    @Column(name = "sicCd_keyword")
-    private String sicCdKeyword;
-
-    @Column(name = "sicNm")
-    private String sicNm;
-
-    @Column(name = "sicNm_keyword")
-    private String sicNmKeyword;
+    @Column(name = "bas_dt")
+    private LocalDate basDt;
 }
